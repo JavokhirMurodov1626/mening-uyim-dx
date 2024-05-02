7c4471c5696c5102ee10d0ed6cecd4e9198a5ea8
Murodov Javohir <javohir.murodov.9238@gmail.com>
2024-05-02 18:05:40 +0500

    create add form reactive form and validators

--- src/app/features/orgs/add-org-form/add-org-form.component.ts
+++ src/app/features/orgs/add-org-form/add-org-form.component.ts
@@ -1,31 +1,68 @@
 import { Component } from '@angular/core';
-import { NgSelectModule, NgOption } from '@ng-select/ng-select';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { OrgsService } from '../../../pages/orgs/orgs.service';
 import { Region } from '../../../types/region.model';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'add-org-form',
   standalone: true,
-  imports: [NgSelectModule],
+  imports: [NgSelectModule, ReactiveFormsModule],
   templateUrl: './add-org-form.component.html',
   styleUrl: './add-org-form.component.css',
 })
-  
 export class AddOrgFormComponent {
-  constructor(private orgsService: OrgsService) {}
   regions!: Region[];
-  
+
+  addOrgForm = this.fb.group({
+    region: [0, [Validators.required]],
+    district: [0, [Validators.required]],
+    orgName: ['', [Validators.required]],
+    supervisor: ['', [Validators.required]],
+    phone: ['', [Validators.required]],
+    tin: [
+      '',
+      [Validators.required, Validators.minLength(9), Validators.maxLength(9)],
+    ],
+  });
+  constructor(private orgsService: OrgsService, private fb: FormBuilder) {}
+
   ngOnInit() {
+    // Fetch regions
     this.orgsService.fetchRegions().subscribe({
       next: (resData) => {
         this.regions = resData.data;
+        console.log(this.regions, 'regions from add org form component');
       },
       error: (err) => {
         console.log(err, 'error from add org form component');
-      }
+      },
     });
   }
 
   selectRegionHandler(region: Region) {
-    
+    this.addOrgForm.patchValue({
+      region: region?.value,
+    });
+  }
+
+  selectDistrictHandler(district: Region) {
+    this.addOrgForm.patchValue({
+      district: district?.value,
+    });
+  }
+
+  onSubmit() {
+    console.log(this.addOrgForm.value);
+    console.log('submitted');
+  }
+
+  checkTin() {
+    console.log('checking tin');
   }
 }
